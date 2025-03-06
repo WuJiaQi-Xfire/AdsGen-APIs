411ce8d6771fa226ce72606ef690faabe1bf85e8
Abby <110977584+yuanners@users.noreply.github.com>
2025-03-06 12:10:30 +0800

    refactor: Create utility class for simplicity

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,8 @@
+//Utility class to simplify the process of constructing and merging class names for use in React components
+
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
